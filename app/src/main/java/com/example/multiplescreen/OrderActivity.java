560956ef077ff84cfb94667838c0e7ea8c480fbe
Blake <02blake.mckenzie@gulllakecs.org>
2021-05-28 10:51:32 -0400

    OrderActivity Source Code

--- app/src/main/java/com/example/multiplescreen/OrderActivity.java
+++ app/src/main/java/com/example/multiplescreen/OrderActivity.java
@@ -1,8 +1,10 @@
-package com.example.multiplescreen;
+package com.codewithharry.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+
+        // Set text inside textview to message
+        TextView textView = findViewById(R.id.orderText);
+        textView.setText(message);
+
+
+
     }
 }
+
